a9dc22a1621345f78519ec6831a15d237f2ef1a2
JCampos05 <juan.camposingsoft@gmail.com>
2026-07-21 13:58:56 -0700

    ey

--- src/app/auth/environment/environment.ts
+++ src/app/auth/environment/environment.ts
@@ -3,6 +3,6 @@ const production = true; // Cambia esto a true para usar la URL de producción
 export const environment = {
   production: production,
   apiUrl: production
-    ? 'https://backend-inventario-5mrd.onrender.com/' // URL de producción (pendiente: URL de Render una vez desplegado el Backend)
+    ? 'https://backend-inventario-5mrd.onrender.com/api' // URL de producción
     : 'http://localhost:3001/api', // URL de desarrollo (Backend corre en el puerto 3001, ver Backend/.env)
 };
